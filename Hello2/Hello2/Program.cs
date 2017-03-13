e7952799147a7fca7e27f47a7233e8175aaaaaa6
marius0604 <marius_0604@yahoo.com>
2017-03-13 17:00:09 +0200

    Add files via upload

---
+++ Hello2/Hello2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name:");
+            string name=Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, "+ name);
+            if (hours > 8)
+                Console.WriteLine("You are well rested.");
+            else
+                Console.WriteLine("You need more sleep.");
+        }
+    }
+}
